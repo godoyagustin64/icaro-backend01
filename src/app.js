17f75432d143c702ffa6da05d98a821470c5dba8
Agustin Godoy <godoyagustinnahuel93@gmail.com>
2022-07-07 21:14:44 -0300

    se añade Titulo

--- src/app.js
+++ src/app.js
@@ -4,6 +4,12 @@ require("dotenv").config()
 const express = require("express");
 const app = express();
 
+app.get("/" , function (solicitud, respuesta) {
+
+    respuesta.send("<h1>Bienvenido</h1>")
+
+})
+
 app.listen(process.env.PUERTO, function() {
-    console.log("Servidor iniciado en puerto: " + process.env.PUERTO);
+    console.log("Servidor iniciado en el puertoo: " + process.env.PUERTO);
 }); 
